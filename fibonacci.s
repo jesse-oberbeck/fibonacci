f8389c0cd5f11386d599bb951ad9cb7c211b88f6
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2017-05-19 21:45:38 -0400

    Prompts when no input is given. Tons of comments added.

--- fibonacci.s
+++ fibonacci.s
@@ -1,92 +1,129 @@
 .intel_syntax noprefix
 
 UsageStatement:
-    .asciz "Usage: ./10times <word>"
+    .asciz "Usage: ./fibonacci <word>"
 
 NumPrint:
     .asciz "\nNumber in Hex: %016lx%016lx%016lx%016lx\n\n"
 
-NumPrintHigh:
-    .asciz "%lx ~~\n"
+ScanPrompt:
+    .asciz "Please enter the desired Fibonacci Number (0 - 300): %d"
+
+ZeroStatement:
+    .asciz "\nNumber in Hex: 0000000000000000000000000000000000000000000000000000000000000000\n\n"
+
+OneStatement:
+    .asciz "\nNumber in Hex: 0000000000000000000000000000000000000000000000000000000000000001\n\n"
 
 .globl main
 main:
 
 # Check for a parameter on the command line
-    cmp edi, 2
-    jne Exit
+    cmp edi, 2      #check argc.
+    jne Prompt      #ask for input if no arg.
 
 # strtol
-    mov rdi, QWORD PTR[rsi + 8]
-    mov rsi, 0
-    mov rdx, 10
-    sub rsp, 8
-    call strtol
-    add rsp, 8
+    mov rdi, QWORD PTR[rsi + 8] #give a location
+    mov rsi, 0      #send the un-needed text to NULL.
+    mov rdx, 10     #base 10.
+    sub rsp, 8      #make room.
+    call strtol     #call function.
+    add rsp, 8      #take room back.
 
-    xor rcx, rcx
-    xor rdx, rdx
+# check if 1 or zero, will also catch invalid input (which yields 0).
+Checks:
+    cmp rsi, 0      #check if input was 0.
+    jle ExitZero    #send to proper exit point.
+    cmp rsi, 1      #check if input was 1.
+    je ExitOne      #send to proper exit point.
+    cmp rsi, 2      #check if answer was actually usable.
+    jge Start       #send to main Fibonacci.
 
-    mov rbx, rax # count number of iterations/determine fibnum
-    sub rbx, 1
+Prompt:
+    mov rdi, OFFSET ScanPrompt
+    call puts       #print the prompt for input.
+    mov rbp, rsp    #save the stack pointer
+    sub rsp, 64     #move the stack pointer to make a new frame.
+    mov rdi, rsp    #put the new stack address in rdi
+    mov rsi, 64     #give a size for the read.
+    mov rdx, stdin  #tell it where to get input.
 
-    xor r8, r8
-    xor r9, r9
-    xor r10, r10
-    xor r11, r11
-    xor r12, r12
-    xor r14, r14
-    xor r15, r15
+    call fgets      #call the function.
 
-    mov rax, 0 # start A
-    mov rsi, 1 # start B
+    mov rdi, rsp    #put the data in place for strtol.
+    mov rsi, 0      #send un-needed text to NULL.
+    mov rdx, 10     #specify base 10
+    sub rsp, 8      #small frame for strtol.
+    call strtol     #call strtol.
+    add rsp, 8      #restore stack from strtol.
+    add rsp, 64     #restore stack from prompt.
+    jmp Checks      #check for numbers less than 2.
 
-    #sub rsp, 16 # Put storage space on stack.
+Start:
+    xor rcx, rcx    #clear the register
+    xor rdx, rdx    #clear the register
+    mov rbx, rax    #count number of iterations/determine fibnum
+    sub rbx, 1      #line up the count with the Fibonacci numbers.
 
+    xor r8, r8      #clear the register
+    xor r9, r9      #clear the register
+    xor r10, r10    #clear the register
+    xor r11, r11    #clear the register
+    xor r12, r12    #clear the register
+    xor r14, r14    #clear the register
+    xor r15, r15    #clear the register
+
+    mov rax, 0      #start A
+    mov rsi, 1      #start B
 
 1:  # Fibonacci Loop.
-    xadd rsi, rax
-    adc rcx, rdx
-    xchg rcx, rdx
+    xadd rsi, rax   #add and switch the registers.
+    adc rcx, rdx    #carryover...
+    xchg rcx, rdx   #chinese fire drill.
 
-    adc r10, r11
-    xchg r10, r11
+    adc r10, r11    #caryover...
+    xchg r10, r11   #chinese fire drill.
 
-    adc r8, r9
-    xchg r8, r9
+    adc r8, r9      #carryover...
+    xchg r8, r9     #chinese fire drill.
 
-    sub rbx, 1
+    sub rbx, 1      #decrement the counter.
 
     jz Exit # Exit loop when count hits zero.
     jmp 1b  # Otherwise go to beginning of loop.
 
-
-
 Exit: 
-
     mov rdi, OFFSET NumPrint #Set up first arg.
-    #xchg rsi, r11 # Change order of args for print.
-    mov r15, rsi #save
-    mov r14, rcx #save
-    mov r13, rdx #save
-    mov r12, r9 #save
+    #xchg rsi, r11  #Change order of args for print.
+    mov r15, rsi    #save.
+    mov r14, rcx    #save.
+    mov r13, rdx    #save.
+    mov r12, r9     #save. now reposition them...
 
-    mov rsi, r12 #arg2 - highest
-    mov rcx, r13 #arg4 - second lowest
-    mov rdx, r11 #arg3 - second highest
-    mov r8, r15 #arg5 - lowest
+    mov rsi, r12    #arg2 - highest
+    mov rcx, r13    #arg4 - second lowest
+    mov rdx, r11    #arg3 - second highest
+    mov r8, r15     #arg5 - lowest
 
+    sub rsp, 8  #make room for printf.
+    call printf #use printf.
+    add rsp, 8  #take the space away.
+    ret         #done!
 
+Usage:
+    mov rdi, OFFSET UsageStatement  #load text
+    call puts   #print text.
+    mov eax, 1  #return value.
+    ret         #exit.
 
-    sub rsp, 8
-    call printf
-    add rsp, 8
+ExitZero:
+    mov rdi, OFFSET ZeroStatement   #load text.
+    call puts   #print text.
+    mov eax, 0  #return value.
+    ret         #exit.
 
-
-
-    #add rsp, 16 # restore stack.
-
-    mov rdi, OFFSET UsageStatement
-    call puts
-    mov eax, 1
-    ret
+ExitOne:
+    mov rdi, OFFSET OneStatement    #load text.
+    call puts   #print text.
+    mov eax, 0  #return value.
+    ret         #exit.
